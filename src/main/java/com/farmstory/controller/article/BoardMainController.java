2b1a34fa4b047366af2f67ab936e584ca9122ce0
hajhi7899 <hajhi7899@gmail.com>
2024-09-08 22:56:34 +0900

    update

--- src/main/java/com/farmstory/controller/article/BoardMainController.java
+++ src/main/java/com/farmstory/controller/article/BoardMainController.java
@@ -1,12 +1,23 @@
 package com.farmstory.controller.article;
 
+import com.farmstory.service.ArticleServicce;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Log4j2
+@RequiredArgsConstructor
 @Controller
 public class BoardMainController {
 
+    private ArticleServicce articleServicce;
+
+    @GetMa
+    public String list(){return "/article/list";}
+    public String write(){return "/article/write";}
+    public String modify(){return "/article/modify";}
+    public String delete(){return "/article/delete";}
 
 
 }
